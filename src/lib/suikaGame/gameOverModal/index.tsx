8d0702c607ff9592cb921f9974a55567c9bb4062
koreacat <kj9966@naver.com>
2023-10-17 16:46:33 +0900

    설문 링크 추가

--- src/lib/suikaGame/gameOverModal/index.tsx
+++ src/lib/suikaGame/gameOverModal/index.tsx
@@ -17,6 +17,7 @@ const GameOverModal = ({ isVisible, onClick, score }: GameOverModalProps) => {
       <span className={cx('text')}>GAME OVER</span>
       <span className={cx('score')}>SCORE: {score}</span>
       <button className={cx('btn')} onClick={onClick}>TRY AGAIN?</button>
+      <a href={'https://forms.gle/QbPDG6rzT4spywyf6'} target='_blank' className={cx('formsLink')}>의견 남기기</a>
     </div>
   )
 }
