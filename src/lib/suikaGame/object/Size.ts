80ca61fbfa60089f26a49db3f38f7d38d1a818d1
koreacat <kj9966@naver.com>
2023-10-19 16:23:09 +0900

    사이즈 조절

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,2 +1,2 @@
-export const getRenderHeight = () => Math.min(window.innerHeight - 100, 700);
-export const getRenderWidth = () => Math.round(getRenderHeight() * 0.5714 - 16);
+export const getRenderHeight = () => Math.min(window.innerHeight - 80, 700);
+export const getRenderWidth = () => Math.round(getRenderHeight() * 0.5714);
