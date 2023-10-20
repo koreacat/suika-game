64b3a0486adaf2da3e275cd7b50b9cc62d4ce676
koreacat <kj9966@naver.com>
2023-10-21 01:17:19 +0900

    공유하기 로직 수정

--- src/lib/suikaGame/gameOverModal/index.tsx
+++ src/lib/suikaGame/gameOverModal/index.tsx
@@ -15,15 +15,40 @@ let timeout: NodeJS.Timeout | null = null;
 const GameOverModal = ({ isVisible, onClick, score }: GameOverModalProps) => {
   const [toastVisible, setToastVisible] = useState(false);
 
-  if(!isVisible) return null;
+  if (!isVisible) return null;
 
-  const clip = () => {
-    timeout && clearTimeout(timeout);
-    navigator.clipboard.writeText(window.location.href);
-    setToastVisible(true);
-    timeout = setTimeout(() => {
-      setToastVisible(false);
-    }, 2800)
+  const share = () => {
+
+    if (navigator.share) {
+      navigator.share({
+        title: '수박 만들기 게임',
+        text: '과일들을 모아 수박을 만들어보세요.',
+        url: 'https://koreacat.github.io/suika-game/',
+      })
+        .then(() => console.log('done'))
+        .catch((error) => console.log(error));
+    } else {
+      timeout && clearTimeout(timeout);
+
+      const urlToCopy = window.location.href;
+
+      // Clipboard API를 지원하는지 확인
+      if (document.queryCommandSupported("copy")) {
+        const input = document.createElement("input");
+        input.value = urlToCopy;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand("copy");
+        document.body.removeChild(input);
+      } else {
+        navigator.clipboard.writeText(urlToCopy)
+      }
+
+      setToastVisible(true);
+      timeout = setTimeout(() => {
+        setToastVisible(false);
+      }, 2800)
+    }
   }
 
   return (
@@ -32,8 +57,8 @@ const GameOverModal = ({ isVisible, onClick, score }: GameOverModalProps) => {
       <span className={cx('score')}>SCORE: {score}</span>
       <button className={cx('btn')} onClick={onClick}>↻ TRY AGAIN?</button>
       <div className={cx('linkArea')}>
-        <a href={'https://forms.gle/QbPDG6rzT4spywyf6'} target='_blank' className={cx('formsLink')}>의견 남기기</a>        
-        <button className={cx('shareaBtn')} onClick={clip}>공유하기</button>        
+        <a href={'https://forms.gle/QbPDG6rzT4spywyf6'} target='_blank' className={cx('formsLink')}>의견 남기기</a>
+        <button className={cx('shareaBtn')} onClick={share}>공유하기</button>
       </div>
       <div className={cx('toastArea', { show: toastVisible })}>🍉URL이 복사되었습니다.</div>
     </div>
